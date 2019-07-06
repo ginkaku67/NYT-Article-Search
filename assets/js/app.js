a7fac4d46456df9ccafb4f8dec59324723a4e03e
ginkaku67 <nshiply76@rocketmail.com>
2019-07-06 13:33:49 -0500

    JS with commented out link (incomplete)

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+$document.ready(){
+$("#SEARCHBUTTON").on("click", function () {
+
+    //variables to store text field values
+    var searchResult = $("#SEARCHTEXTFIELD").val();
+    var numberResult = $("#NUMBERTEXTFIELD").val();
+    var startYearResult = $("#SDATETEXTFIELD").val();
+    var endYearResult = $("#EDATETEXTFIELD").val();
+
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchResult + "&api-key=XAuF7o3HfClOzfN1qqtpc0G2UuoqWqGz";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+        console.log(response);
+
+
+
+        for (i = 0; i < numberResult; i++) {
+
+            //This is just to correctly display the first result as #1 instead of #0
+            var counter = parseInt([i]) + 1;
+
+            var articleResult = $("<h2>").text("#" + counter + " " + response.response.docs[i].headline.main);
+            $("#DISPLAYDIV").append(articleResult);
+
+        }
+
+    });
+});
+$("#CLEARBUTTON").on("click", function () {
+    $("#DISPLAYDIV").empty();
+});
+};
